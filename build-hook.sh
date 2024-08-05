17bbda5b1b9c1b1809aa73461b8f478022306492
aaron <616915054@qq.com>
2024-08-05 17:06:06 +0800

    update

--- build-hook.sh
+++ build-hook.sh
@@ -1,4 +1,6 @@
 TACHE_API_PATH=/Users/chenghao/www/aaron/comiur/teach-api
 cp -rf dist/* $TACHE_API_PATH/public
-
-echo 11111
+cd $TACHE_API_PATH
+git add .
+git commit -m "web build commit";
+git push origin
